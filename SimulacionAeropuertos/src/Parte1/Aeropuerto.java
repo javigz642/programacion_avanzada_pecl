97079f3c4969c0875aebb69d8d9d5d9db4be45c3
javigz642 <edisonjaviertg@gmail.com>
2024-03-26 18:46:08 +0100

    Create Aeropuerto.java

---
+++ SimulacionAeropuertos/src/Parte1/Aeropuerto.java
@@ -0,0 +1,14 @@
+package Parte1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ediso
+ */
+public class Aeropuerto {
+    
+}
