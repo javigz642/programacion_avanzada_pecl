49dcdfa05504f5b4a5cd4166dc937929b32cf6b8
javigz642 <edisonjaviertg@gmail.com>
2024-04-28 19:53:18 +0200

    push

--- SimulacionAeropuertos/src/Parte1/Aeropuerto.java
+++ SimulacionAeropuertos/src/Parte1/Aeropuerto.java
@@ -13,19 +13,16 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author ediso
  */
 public class Aeropuerto extends Thread {
-    
-    protected Ciudad ciudad; 
-    
+
+    protected Ciudad ciudad;
+
     protected Hangar hangar = new Hangar(this);
     protected AreaEstacionamiento areaEstacionamiento = new AreaEstacionamiento(this);
     protected Taller taller = new Taller(this);
-    
+
     private int personasDentro;
-    
+
     private Lock puertaSalida = new ReentrantLock();
-    private Condition vacio = puertaSalida.newCondition();
-    
-    
 
     public Aeropuerto(int personasDentro, Ciudad ciudad) {
 
@@ -36,31 +33,31 @@ public class Aeropuerto extends Thread {
     public void recogerPasajerosAutobus(Autobus a) {
 
         int pasajerosParada;
-        do{
-         pasajerosParada = (int) (Math.random() * 51);
-        }while(pasajerosParada > personasDentro);
-        
+        do {
+            pasajerosParada = (int) (Math.random() * 51);
+        } while (pasajerosParada > personasDentro);
+
         puertaSalida.lock();
-        
+
         try {
-            System.out.println("Hay "+  personasDentro+ " dentro");
-            while (personasDentro == 0) { 
-                System.out.println("El autobus " + a.getIdentificador() + "se queda esperando a mas pasajeros");
-                vacio.await(); 
+            System.out.println("Hay " + personasDentro + " dentro");
+
+            if (personasDentro > 0) {
+                personasDentro -= pasajerosParada;
+                System.out.println("El autobus " + a.getIdentificador() + " va a recoger a " + pasajerosParada + " personas.");
+
             }
-            
-            personasDentro -= pasajerosParada;
-            System.out.println("El autobus " + a.getIdentificador() + " va a recoger a " + pasajerosParada + " personas.");
             Thread.sleep((int) (Math.random() * 3000) + 2001);
         } catch (Exception e) {
-            
-        }finally{
+
+        } finally {
             puertaSalida.unlock();
-        }  
+            
+        }
     }
 
     public void irCiudadAutobus(Autobus a) {
-        
+
         try {
             System.out.println("El autobus " + a.getIdentificador() + " va hacia la ciudad de " + ciudad.getNombre());
             Thread.sleep((int) (Math.random() * 5000) + 5001);
@@ -68,29 +65,28 @@ public class Aeropuerto extends Thread {
         } catch (Exception e) {
         }
     }
-    
-    public void bajarPasajerosAutobus(Autobus a){
+
+    public void bajarPasajerosAutobus(Autobus a) {
         puertaSalida.lock();
         try {
             personasDentro += a.getPasajeros();
         } catch (Exception e) {
-        }finally{
+        } finally {
             puertaSalida.unlock();
         }
     }
-    
-    public int recogerPasajerosAvion(int pasajeros) throws InterruptedException{
-        
+
+    public int recogerPasajerosAvion(int pasajeros) throws InterruptedException {
+
         int pasajerosCogidos;
-        
-        if (personasDentro >= pasajeros){
+
+        if (personasDentro >= pasajeros) {
             personasDentro -= pasajeros;
             pasajerosCogidos = pasajeros;
-        }
-        else{
+        } else {
             pasajerosCogidos = personasDentro;
             personasDentro = 0;
-    
+
         }
         Thread.sleep((int) (Math.random() * 2000) + 1001);
         return pasajerosCogidos;
@@ -99,7 +95,5 @@ public class Aeropuerto extends Thread {
     public int getPersonasDentro() {
         return personasDentro;
     }
-    
-    
-            
+
 }
