9e8a17dce14299adbfe65e1fe9d16789efdc8b64
javigz642 <edisonjaviertg@gmail.com>
2024-05-04 17:14:34 +0200

    paso pistas con sout unico cuando se para y reanuda

--- SimulacionAeropuertos/src/Clases/PasoPistas.java
+++ SimulacionAeropuertos/src/Clases/PasoPistas.java
@@ -11,65 +11,65 @@ import java.util.concurrent.locks.ReentrantLock;
 // La clase Paso define un cerrojo con un Condition para la variable booleana cerrado
 // que es comprobada por un proceso.
 // Si vale false(abierto) el proceso puede continuar. Si es true(cerrado) el proceso se detiene
-public class PasoPistas
-{
-    private boolean cerrado[]={false,false,false,false};
+public class PasoPistas {
+
+    private boolean cerrado[] = {false, false, false, false};
     private Lock cerrojo = new ReentrantLock();
     private Condition parar = cerrojo.newCondition();
+    private boolean soloUnHilo[] = {true, true, true, true};
 
-    public void mirar(int numero)
-    {
-        try{
-            
+    public void mirar(int numero) {
+        try {
+
             cerrojo.lock();
             //imprimir();
 
-            while(cerrado[numero])
-            {
-                try
-                {
+            while (cerrado[numero]) {
+                try {
                     parar.await();
-                } catch(InterruptedException ie){ }
+                } catch (InterruptedException ie) {
+                }
             }
-        }
-        finally {
+        } finally {
             cerrojo.unlock();
         }
     }
-    
-    public void abrir(int numero)
-    {
-        
-        try
-        {
-            cerrojo.lock();
-            cerrado[numero]=false;
-            parar.signalAll();
-        }
-        finally
-        {
-            cerrojo.unlock();
-        }
-    }
-    
-    
-    public void cerrar(int numero)
-    {
 
-        try
-        {
+    public void abrir(int numero) {
+
+        try {
             cerrojo.lock();
-            cerrado[numero]=true;
-        }
-        finally
-        {
+            if (!soloUnHilo[numero]) {
+                System.out.println("Reanudando pista numero " + (numero + 1));
+                cerrado[numero] = false;
+                soloUnHilo[numero] = true;
+                parar.signalAll();
+            }
+        } finally {
             cerrojo.unlock();
         }
     }
-    public void imprimir(){
+
+    public void cerrar(int numero) {
+
+        try {
+
+            cerrojo.lock();
+            if (soloUnHilo[numero]) {
+                System.out.println("Parando pista numero " + (numero + 1));
+                cerrado[numero] = true;
+                soloUnHilo[numero] = false;
+            }
+
+        } finally {
+            cerrojo.unlock();
+        }
+    }
+
+    public void imprimir() {
         String aux = "";
         for (int i = 0; i < cerrado.length; i++) {
-            aux += (cerrado[i]?"true":"false") + "/";
+            aux += (cerrado[i] ? "true" : "false") + "/";
         }
         System.out.println(aux);
     }
